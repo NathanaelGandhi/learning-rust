4aab93570df6210944dd1038a2ac745f56958e5b
Nathanael Gandhi <nat11public@gmail.com>
2023-08-24 13:29:35 +0000

    wip: initial 20230824

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+// using https://www.youtube.com/watch?v=BpPEoZW5IiY
+
 fn main() {
     println!("Hello, world!");
+    let a_number: i32 = 6;
+    println!("Printing a number: {}", a_number);
+    my_function();
+    println!("a number + 10: {}", add(a_number, 10))
+}
+
+fn my_function(){
+    println!("now in 'my_function'");
+}
+
+fn add(a: i32, b: i32)-> i32{
+    return a + b;
 }
