acd690dec1b581ce825917da027c428726d1b96c
Nathanael Gandhi <nat11public@gmail.com>
2023-08-26 04:08:20 +0000

    feat: guessing game

---
+++ src/guessing_game.rs
@@ -0,0 +1,44 @@
+// https://doc.rust-lang.org/stable/book/ch02-00-guessing-game-tutorial.html
+
+use std::cmp::Ordering;
+use std::io;
+
+use rand::Rng;
+
+pub fn run_game() {
+    println!("--- Guessing Game ---");
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    // println!("The secret number is: {secret_number}");
+
+    loop {
+        println!("Please input your guess.");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please ensure your guess is an integer");
+                continue;
+            }
+        };
+
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
+}
